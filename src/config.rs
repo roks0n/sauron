fcd288aadeac5fc45c0f8900235d76b04f3a36d1
roks0n <me@roks0n.com>
2022-03-09 15:15:22 +0000

    add default trait to Config struct

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,7 @@ use crate::utils::get_homedir;
 const CONFIG_PATH: &str = ".config/sauron-watcher/";
 const CONFIG_FILE: &str = "config.toml";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct Config {
     pub statsd_host: String,
     pub statsd_prefix: String,
@@ -24,11 +24,10 @@ pub fn get_config_file() -> File {
     let config_file_path = &format!("{}/{}/{}", home_dir, CONFIG_PATH, CONFIG_FILE);
 
     if !Path::new(config_file_path).exists() {
-        file = File::create(config_file_path).unwrap();
-    } else {
-        file = File::options().read(true).write(true).open(config_file_path).unwrap();
+        File::create(config_file_path).unwrap();
     }
 
+    file = File::options().read(true).write(true).open(config_file_path).unwrap();
     file
 }
 
@@ -37,6 +36,7 @@ pub fn get_config() -> Config {
     let mut config_string= String::new();
     file.read_to_string(&mut config_string).unwrap();
 
-    let config_data: crate::config::Config = toml::from_str(&mut config_string).unwrap();
+    let config_data: crate::config::Config = toml::from_str(&mut config_string)
+        .unwrap_or_default();
     config_data
 }
